628e00e634cd0000100d479027389515bd724605
youngsoo77 <ys555han@gmail.com>
2019-08-05 04:54:27 +0900

    add blog admin page

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Category, Post, Comment
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'created_at', 'updated_at', 'is_published']
+    list_display_links = ['id', 'name', ]
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'get_category', 'title', 'get_author_name', 'created_at', 'updated_at', 'is_published']
+    list_display_links = ['id', 'title', ]
+
+    def get_author_name(self, obj):
+        return obj.author.username if obj.author else None
+
+    def get_category(self, obj):
+        return obj.category.name if obj.category else None
+
+    get_author_name.admin_order_field = 'author'
+    get_author_name.short_description = 'Author Name'
+    get_category.admin_order_field = 'category'
+    get_category.short_description = 'Category'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'short_body'] + [field.name for field in Comment._meta.get_fields() if
+                                           field.name not in ['id', 'body', 'post']]
+    list_display_links = ['short_body']
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.template.defaultfilters import truncatechars
 from django.utils.translation import gettext_lazy as _
 
 from base.models import AutoCreatedUpdatedModel
@@ -17,15 +18,28 @@ class BaseBlogModel(models.Model):
 class Category(AutoCreatedUpdatedModel, BaseBlogModel):
     name = models.CharField(max_length=255, null=True, blank=True, help_text=_('카테고리'))
 
+    def __str__(self):
+        return self.name if self.name else "empty name"
+
 
 class Post(AutoCreatedUpdatedModel, BaseBlogModel):
-    categories = models.ManyToManyField(Category, related_name='posts')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     author = models.ForeignKey(USER, on_delete=models.CASCADE)
     title = models.CharField(max_length=255, null=True, blank=True, help_text=_('글 제목'))
     body = models.TextField(null=True, blank=True, help_text=_('글 내용'))
+
+    def __str__(self):
+        return f"[{self.category}] {self.title if self.title else ''}"
 
 
 class Comment(AutoCreatedUpdatedModel, BaseBlogModel):
     author = models.ForeignKey(USER, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     body = models.TextField(null=True, blank=True, help_text=_('글 내용'))
+
+    @property
+    def short_body(self):
+        return truncatechars(self.body, 20) if self.body else ""
+
+    def __str__(self):
+        return f"{self.short_body} ({self.author.username})"
